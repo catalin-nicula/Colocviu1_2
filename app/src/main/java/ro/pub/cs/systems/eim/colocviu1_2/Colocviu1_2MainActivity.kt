35703c1c0e5df9b6fdbc3d4a76423860f3151cf5
catalin-nicula <catalin.nicula@stud.acs.upb.ro>
2024-11-13 17:22:56 +0200

    finished task up to C.2 including

--- app/src/main/java/ro/pub/cs/systems/eim/colocviu1_2/Colocviu1_2MainActivity.kt
+++ app/src/main/java/ro/pub/cs/systems/eim/colocviu1_2/Colocviu1_2MainActivity.kt
@@ -14,12 +14,15 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import ro.pub.cs.systems.eim.colocviu1_2.Constants.All_TERMS_STATE
+import ro.pub.cs.systems.eim.colocviu1_2.Constants.SUM_RESULT_STATE
 
 class Colocviu1_2MainActivity : AppCompatActivity() {
     lateinit var nextTermEditText: EditText
     lateinit var allTermViewText: TextView
     lateinit var addButton: Button
     lateinit var computeButton: Button
+    var sumState: Int = 0
+    var oldAllTerms:String = ""
 
     private val buttonClickListener = ButtonClickListener()
 
@@ -30,6 +33,7 @@ class Colocviu1_2MainActivity : AppCompatActivity() {
             when (view.id) {
                 R.id.AddButton -> {
                     if(nextTermEdit != ""){
+                        oldAllTerms = allTermView
                         allTermView += " + $nextTermEdit"
                         allTermViewText.setText(allTermView)
                     }
@@ -52,15 +56,38 @@ class Colocviu1_2MainActivity : AppCompatActivity() {
         val activityResultsLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
             val data: Intent? = result.data
             val sum = data?.getIntExtra("SUM_RESULT", 0) ?: 0
+            sumState = sum;
             if (result.resultCode == Activity.RESULT_OK) {
-                Toast.makeText(this, "The activity returned with sum: $sum", Toast.LENGTH_LONG).show()
+                Toast.makeText(this, "The activity returned with sum: $sumState", Toast.LENGTH_LONG).show()
             }
         }
 
         computeButton.setOnClickListener {
-            val intent = Intent(this, Colocviu1_2SecondaryActivity::class.java)
-            intent.putExtra(All_TERMS_STATE, allTermViewText.text.toString())
-            activityResultsLauncher.launch(intent)
+            if(oldAllTerms == allTermViewText.text.toString()){
+                Toast.makeText(this, "The activity returned with sum(no calc): $sumState", Toast.LENGTH_LONG).show()
+            }
+            else{
+                val intent = Intent(this, Colocviu1_2SecondaryActivity::class.java)
+                intent.putExtra(All_TERMS_STATE, allTermViewText.text.toString())
+                activityResultsLauncher.launch(intent)
+                oldAllTerms = allTermViewText.text.toString()
+            }
+
         }
+    }
+
+    override fun onSaveInstanceState(savedInstanceState: Bundle) {
+        super.onSaveInstanceState(savedInstanceState)
+        savedInstanceState.putInt(SUM_RESULT_STATE, sumState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        if (savedInstanceState.containsKey(SUM_RESULT_STATE)) {
+            sumState = savedInstanceState.getInt(SUM_RESULT_STATE)
+        } else {
+            sumState = 0
+        }
+        Toast.makeText(this, "The activity returned with sum: $sumState", Toast.LENGTH_LONG).show()
     }
 }
--- app/src/main/java/ro/pub/cs/systems/eim/colocviu1_2/Constants.kt
+++ app/src/main/java/ro/pub/cs/systems/eim/colocviu1_2/Constants.kt
@@ -3,5 +3,6 @@ package ro.pub.cs.systems.eim.colocviu1_2
 object Constants {
     const val tag = "colocviu1_2"
     const val All_TERMS_STATE = "All_TERMS_STATE"
+    const val SUM_RESULT_STATE = "SUM_RESULT_STATE"
     const val SUM_RESULT = "SUM_RESULT"
 }
