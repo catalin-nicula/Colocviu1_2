11fcbd045bc80b1e542c8d0e3ed21232bb52b437
catalin-nicula <catalin.nicula@stud.acs.upb.ro>
2024-11-13 16:29:03 +0200

    finished tasks for A

---
+++ app/src/main/java/ro/pub/cs/systems/eim/colocviu1_2/Colocviu1_2MainActivity.kt
@@ -0,0 +1,14 @@
+package ro.pub.cs.systems.eim.colocviu1_2
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Colocviu1_2MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_practical_test01_2_main)
+    }
+}
